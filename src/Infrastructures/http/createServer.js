da40fd91724fdb1066c78c7c91fe2c1c42fce2eb
BillyTan <www.billy39@gmail.com>
2024-11-28 17:02:20 +0700

    try to push say hello world feature again

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -25,7 +25,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/',
     handler: () => ({ value: 'Hello World!' })
-  })
+  });
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
