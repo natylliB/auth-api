45109720fde6a5be4ccb930e39a429a1d98cc9b2
BillyTan <www.billy39@gmail.com>
2024-11-28 12:10:03 +0700

    changed the create server config options

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -3,13 +3,11 @@ const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
 const users = require('../../Interfaces/http/api/users');
 const authentications = require('../../Interfaces/http/api/authentications');
-const config = require('../../Commons/config');
 
 const createServer = async (container) => {
   const server = Hapi.server({
-    port: config.app.port,
-    host: config.app.host,
-    debug: config.app.debug,
+    port: process.env.PORT,
+    host: process.env.HOST,
   });
 
   await server.register([
